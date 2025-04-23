1ed03f5426e61e6768f6035ac2ec8e3bd4cad9a8
Davy cosmo <davydevcosmo@gmail.com>
2025-04-22 22:33:51 -0300

    Feat: favicon do head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,23 +17,7 @@ export const metadata: Metadata = {
   description: "Desenvolvedor front-end Freelancer, especializado em React, Next.js e desenvolvimento web moderno",
   authors: [{ name: "Davy", url: "https://Davycosmo.com" }],
   keywords: ["portfolio", "desenvolvedor", "react", "next.js", "criação de sites", "front-end", "davy cosmo"],
-  icons: {
-    icon: [
-      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon/favicon.ico", sizes: "any" },
-    ],
-    apple: [
-      { url: "/favicon/apple-touch-icon.png", sizes: "180x180" }
-    ],
-    other: [
-      {
-        rel: "mask-icon",
-        url: "/favicon/safari-pinned-tab.svg",
-        color: "#5bbad5"
-      }
-    ]
-  },
+
   manifest: "/favicon/site.webmanifest",
   themeColor: "#ffffff",
   appleWebApp: {
@@ -54,6 +38,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className="antialiased">
         {children}
