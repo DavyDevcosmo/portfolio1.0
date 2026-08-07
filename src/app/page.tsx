36c9c04d36762c9192d0f9ae35fdd6d126f411f0
Davy cosmo <davydevcosmo@gmail.com>
2026-08-06 23:05:00 -0300

    Feat: adição do componente Navbar à página inicial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import About from "./componets/about";
 import CardsTechnology from "./componets/cardsTechnology";
 import Footer from "./componets/footer";
+import Navbar from "./componets/Navbar";
 import Presentation from "./componets/presentation";
 import Projects from "./componets/projects;";
 
 export default function Home() {
 	return (
 		<>
+			<Navbar />
 			<header>
 				<Presentation />
 			</header>
